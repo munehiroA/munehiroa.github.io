3ca559cdb4e29b525d9e1430bbd8ce420fac5997
munehiroA <m.asally@warwick.ac.uk>
2026-01-01 10:36:06 +0000

    Delete js directory

--- js/_vti_cnf/jquery.bxslider.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|05 Apr 2014 07:14:38 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|YODA\\Munehiro
-vti_modifiedby:SR|YODA\\Munehiro
-vti_timecreated:TR|05 Apr 2014 07:14:38 -0000
-vti_cacheddtm:TX|05 Apr 2014 07:14:38 -0000
-vti_filesize:IR|50573
-vti_backlinkinfo:VX|
